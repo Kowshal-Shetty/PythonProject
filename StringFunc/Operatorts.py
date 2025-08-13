876ba33745f9f7a869c0326e9ba340e8ce4ae1c5
Kshetty <kowshalshetty@gmail.com>
2025-08-13 08:41:50 -0700

    Added 2 pgrms

---
+++ StringFunc/Operatorts.py
@@ -0,0 +1,5 @@
+X=input("Enter num1")
+Y=input("Enter num2")
+Z=input("Enter num3")
+
+print(int(X)+int(Y)+int(Z))
---
+++ StringFunc/QuotientReminder.py
@@ -0,0 +1,8 @@
+X = input("Enter num1")
+Y = input("Enter num2")
+X=int(X)
+Y=int(Y)
+Q=X/Y
+print(Q)
+R=X%Y
+print(R)
