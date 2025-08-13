b19b03cc9961e648b97882d26530fa14d99b9fd1
Kshetty <kowshalshetty@gmail.com>
2025-08-13 06:11:42 -0700

    Added Literals

---
+++ Literals.py
@@ -0,0 +1,17 @@
+age = 65
+age+1
+print(age)
+age = age + 1
+print(age)
+
+
+name = input("What is your name? ")
+print(name)
+
+
+num1 = input("Enter num1 value")
+num2 = input("Enter num2 value")
+num1= int(num1)
+num2 = int(num2)
+sumval = num1 + num2
+print(sumval)
